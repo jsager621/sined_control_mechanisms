c7736d1cb57d7b93948c0b164f3df4e694809a0b
CarWegkamp <c.wegkamp@tu-braunschweig.de>
2024-01-18 18:50:45 +0100

    add prosumer method to optimize day schedule (WIP)

--- src/participant.py
+++ src/participant.py
@@ -1,5 +1,12 @@
 from mango import Agent
-import pandapower
+import pyomo.environ as pyo
+
+GRANULARITY = 0.25
+DAY_STEPS = range(int(24 / GRANULARITY))
+DAY_STEPS_PLUS_1 = range(int(24 / GRANULARITY + 1))
+ELEC_PRICE = 0.35
+FEEDIN_TARIFF = 0.07
+
 
 class NetParticipant(Agent):
     def __init__(self, container):
@@ -22,6 +29,71 @@ class NetParticipant(Agent):
 
     def _handle_schedule_message(self, content, meta):
         pass
+
+    def calc_opt_day(
+        self, load_profile, generation_profile, ev_profile, hp_profile, bss_vals
+    ):
+        model = pyo.ConcreteModel()
+
+        # add optimization variables
+        model.x_grid_load = pyo.Var(DAY_STEPS, domain=pyo.NonNegativeReals)
+        model.x_grid_feedin = pyo.Var(DAY_STEPS, domain=pyo.NonNegativeReals)
+        model.x_bss_p_charge = pyo.Var(DAY_STEPS, domain=pyo.NonNegativeReals)
+        model.x_bss_p_discharge = pyo.Var(DAY_STEPS, domain=pyo.NonNegativeReals)
+        model.x_bss_e = pyo.Var(DAY_STEPS_PLUS_1, domain=pyo.NonNegativeReals)
+
+        # add objective function
+        model.OBJ = pyo.Objective(
+            expr=ELEC_PRICE * model.x_grid_load[:]
+            - FEEDIN_TARIFF * model.x_grid_feedin[:]
+        )
+
+        # add constraints: balance
+        model.Constraint1 = pyo.Constraint(
+            expr=load_profile
+            + hp_profile
+            + ev_profile
+            + model.x_grid_feedin[:]
+            + model.x_bss_p_charge[:]
+            == generation_profile + model.x_grid_load[:] + model.x_bss_p_discharge[:]
+        )
+
+        # add constraints: ev
+
+        # add constraints: hp
+
+        # add constraints: bss
+        opt_prob.addConstr(
+            (opt_vars["bat_energy_kWh"][0] == 0)
+        )  # BSS empty at the beginning
+        for t in DAY_STEPS:
+            opt_prob.addConstrs(
+                (
+                    opt_vars["bat_energy_kWh"][t + 1]
+                    == opt_vars["bat_energy_kWh"][t]
+                    + (
+                        opt_vars["bat_charge_kW"][t] * bss_vals["eff"]
+                        - opt_vars["bat_discharge_kW"][t] / bss_vals["eff"]
+                    )
+                    * GRANULARITY
+                    for t in DAY_STEPS
+                ),
+            )
+        opt_prob.addConstrs(
+            (
+                opt_vars["bat_energy_kWh"][t] <= bss_vals["cap_kWh"]
+                for t in DAY_STEPS_PLUS_1
+            ),
+        )
+        opt_prob.addConstrs(
+            (opt_vars["bat_charge_kW"][t] <= bss_vals["power_kW"] for t in DAY_STEPS),
+        )
+        opt_prob.addConstrs(
+            (
+                opt_vars["bat_discharge_kW"][t] <= bss_vals["power_kW"]
+                for t in DAY_STEPS
+            ),
+        )
 
     def run(self):
         # to proactively do things
