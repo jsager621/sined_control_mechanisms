d35f6e8d37c8b35d2320449acfc58708008a547d
CarWegkamp <c.wegkamp@tu-braunschweig.de>
2024-01-18 18:21:36 +0100

    add grid methods: init, inputs, powerflow

--- src/central_instance.py
+++ src/central_instance.py
@@ -1,7 +1,12 @@
 from mango import Agent
-import pandapower
+import pandapower as pp
+import pandapower.networks as ppnet
+
 
 class CentralInstance(Agent):
+    grid = None  # pandapower net object
+    grid_results = {}  # results of grid from powerflow calculation
+
     def __init__(self, container):
         # We must pass a reference of the container to "mango.Agent":
         super().__init__(container)
@@ -16,6 +21,37 @@ class CentralInstance(Agent):
 
     def _send_schedule_message(self, target, content):
         pass
+
+    def init_grid(self, grid_name: str):
+        if grid_name == "kerber_dorfnetz":
+            self.grid = ppnet.create_kerber_dorfnetz()
+        elif grid_name == "kerber_landnetz":
+            self.grid = ppnet.create_kerber_landnetz_kabel_1()
+        else:
+            print(f"Grid {self.aid} could not be created.")
+            self.grid = None
+
+    def set_inputs(self, data_for_buses):
+        """Set active/reactive power values for the grid load buses."""
+        for name_bus, power_values in data_for_buses.items():
+            element = getattr(self.grid, name_bus)
+
+            element.at[name_bus, "p_mw"] = power_values[0]  # active power value
+            element.at[name_bus, "q_mvar"] = power_values[1]  # reactive power value
+
+    def calc_grid_powerflow(self):
+        pp.runpp(
+            self.grid,
+            numba=False,
+            calculate_voltage_angles=False,
+        )
+
+        self.grid_results = {}
+        if not self.grid.res_bus.empty:  # powerflow converged
+            for i_bus in range(len(self.grid.res_bus)):
+                self.grid_results[i_bus] = self.grid.res_bus.loc[i_bus]
+            for i_line in range(len(self.grid.res_line)):
+                self.grid_results[i_line] = self.grid.res_line.loc[i_line]
 
     def run(self):
         # to proactively do things
