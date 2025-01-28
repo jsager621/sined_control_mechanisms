af6c80b3a64a3408993f90dae6228b4ea637cc01
CarWegkamp <c.wegkamp@tu-braunschweig.de>
2025-01-28 19:06:50 +0100

    bugfix and enhance output generation, esp. plots

--- src/plot_results_compare.py
+++ src/plot_results_compare.py
@@ -12,8 +12,8 @@ from plot_results import get_line_load, get_vm_pu, get_agents_res
 # import copy
 
 GRAN = 96
-DAY_START = 6.3
-DAY_END = 8.7
+DAY_START = 40.75
+DAY_END = 40.99
 IDX_DAYS_PLOT = [int(GRAN * DAY_START), int(GRAN * DAY_END)]
 
 
@@ -86,17 +86,23 @@ def comp_results_line(res_dict):
     )
 
     # profile for exemplary day
-    fig = plt.figure(figsize=(10, 4))
-    for name, res_sim in res_dict.items():
-        plt.plot(res_sim["df"]["trafo 1"], label=name)
-    plt.xlabel("Time")
-    plt.ylabel("Transformer loading, in %")
-    plt.xlim(IDX_DAYS_PLOT[0], IDX_DAYS_PLOT[1])
-    plt.legend()
-    fig.get_axes()[0].set_rasterized(True)
-    plt.savefig(
-        os.path.join("outputs", "comp", "line_load_profile.png"), dpi=300, format="png"
-    )
+    if len(res_dict) < 10:
+        fig = plt.figure(figsize=(10, 4))
+        for name, res_sim in res_dict.items():
+            plt.plot(res_sim["df"]["trafo 1"], label=name)
+        plt.xlabel("Simulation time step")
+        plt.ylabel("Transformer loading, in %")
+        plt.xlim(IDX_DAYS_PLOT[0], IDX_DAYS_PLOT[1])
+        plt.ylim(0, 125)
+        plt.axhline(y=100, color="r", linestyle="--", linewidth=1)
+        plt.legend(loc="upper left")
+        fig.get_axes()[0].set_rasterized(True)
+        plt.savefig(
+            os.path.join("outputs", "comp", "line_load_profile.png"),
+            dpi=300,
+            format="png",
+            bbox_inches="tight",
+        )
 
 
 def comp_results_bus(res_dict):
@@ -134,8 +140,8 @@ def comp_results_bus(res_dict):
     list_ovl_sum_up = []
     list_ovl_sum_lw = []
     for name, res_sim in res_dict.items():
-        list_vio_up.append(sum(res_sim["num_ovl_up_list"]))
-        list_vio_low.append(sum(res_sim["num_ovl_lw_list"]))
+        list_vio_up.append(max(res_sim["num_ovl_up_list"]))
+        list_vio_low.append(min(res_sim["num_ovl_lw_list"]))
         list_ovl_sum_up.append(res_sim["ovl_sum_ov"])
         list_ovl_sum_lw.append(res_sim["ovl_sum_lw"])
 
@@ -174,6 +180,27 @@ def comp_results_bus(res_dict):
         format="png",
         bbox_inches="tight",
     )
+
+    # profile for exemplary day
+    if len(res_dict) < 10:
+        fig = plt.figure(figsize=(10, 4))
+        plt.axhline(y=1.05, color="r", linestyle="--", linewidth=1)
+        plt.axhline(y=0.95, color="r", linestyle="--", linewidth=1)
+        plt.axhline(y=1, color="grey", linestyle="--", linewidth=1)
+        for name, res_sim in res_dict.items():
+            plt.plot(res_sim["df"]["KV_3_16"], label=name)
+        plt.xlabel("Simulation time step")
+        plt.ylabel("Bus voltage magnitude, in p.u.")
+        plt.xlim(IDX_DAYS_PLOT[0], IDX_DAYS_PLOT[1])
+        plt.ylim(0.945, 1.005)
+        plt.legend(loc="upper left")
+        fig.get_axes()[0].set_rasterized(True)
+        plt.savefig(
+            os.path.join("outputs", "comp", "bus_vm_profile.png"),
+            dpi=300,
+            format="png",
+            bbox_inches="tight",
+        )
 
 
 def comp_results_agents(res_dict):
@@ -240,7 +267,7 @@ def comp_results_agents(res_dict):
 def comp_results_table(sim_res):
     column_names = [
         "Max. Line loading, in %",
-        "Min. Line Loading, in %",
+        "Min. Line loading, in %",
         "#Ovl loading",
         "Sum overload work, in kWh",
         "Max. Voltage, in p.u.",
@@ -266,10 +293,344 @@ def comp_results_table(sim_res):
         df_res.at[sim_name, "Sum overload work, in kWh"] = res_dict["ovl_work_kWh"]
 
     # insert bus results
+    for sim_name, res_dict in sim_res["bus"].items():
+        df_res.at[sim_name, "Max. Voltage, in p.u."] = res_dict["max_val"]
+        df_res.at[sim_name, "Min. Voltage, in p.u."] = res_dict["min_val"]
+        df_res.at[sim_name, "#Ovl voltage upper"] = max(res_dict["num_ovl_up_list"])
+        df_res.at[sim_name, "#Ovl voltage lower"] = min(res_dict["num_ovl_lw_list"])
+        df_res.at[sim_name, "Sum over-voltage, in p.u."] = res_dict["ovl_sum_ov"]
+        df_res.at[sim_name, "Sum under-voltage, in p.u."] = res_dict["ovl_sum_lw"]
 
     # insert agent results
+    for sim_name, res_dict in sim_res["agents"].items():
+        df_res.at[sim_name, "Avrg. Energy Prosumer, in kWh"] = res_dict["mean_energy"]
+        df_res.at[sim_name, "Avrg. Cost Prosumer, in EUR"] = res_dict["mean_cost"]
+        df_res.at[sim_name, "Avrg. Self-consumption Prosumer, in %"] = res_dict[
+            "mean_sc"
+        ]
+        df_res.at[sim_name, "Avrg. Self-sufficiency Prosumer, in %"] = res_dict[
+            "mean_ss"
+        ]
 
     df_res.to_excel(os.path.join("outputs", "comp", "table.xlsx"))
+
+
+def comp_results_plots_violations(sim_res):
+    # plot for comparison of number of violations
+    fig, ax1 = plt.subplots(figsize=(10, 4))
+    ax1.grid()
+    x_coord = np.arange(len(sim_res["bus"].keys()))
+    # voltage values
+    num_v = [max(res_dict["num_ovl_up_list"]) for res_dict in sim_res["bus"].values()]
+    col_up = (102 / 255, 180 / 255, 211 / 255)
+    col_lw = (0 / 255, 83 / 255, 116 / 255)
+    ax1.scatter(
+        x_coord, num_v, label="Voltage - upper limit", color=col_up, marker="^", s=120
+    )
+    num_v = [-min(res_dict["num_ovl_lw_list"]) for res_dict in sim_res["bus"].values()]
+    ax1.scatter(
+        x_coord, num_v, label="Voltage - lower limit", color=col_lw, marker="v", s=120
+    )
+    # loading values
+    num_v = [res_dict["num_ovl_sum"] for res_dict in sim_res["line"].values()]
+    ax1.scatter(
+        x_coord, num_v, label="Loading", color=(109 / 255, 131 / 255, 0 / 255), s=100
+    )
+    # plt.ylim((min(list_ss) - 0.1, max(max(list_ss) + 0.3, 1)))
+    ax1.set_xlabel("")
+    ax1.set_ylabel("Number of violations")
+    ax1.tick_params(axis="y")
+    ax1.set_xticks(x_coord)
+    ax1.set_xticklabels(sim_res["bus"].keys(), rotation=45, ha="right")
+    ax1.legend(loc="upper right")
+    # ax1.set_rasterized(True)
+    plt.savefig(
+        os.path.join("outputs", "comp", "comp_plot_num.png"),
+        dpi=300,
+        format="png",
+        bbox_inches="tight",
+    )
+
+    # plot for comparison of aggregated violation
+    fig, ax1 = plt.subplots(figsize=(10, 4.5))
+    ax1.grid()
+    x_coord = np.arange(len(sim_res["bus"].keys()))
+    # first axis with voltage values
+    col = (102 / 255, 180 / 255, 211 / 255)
+    num_v = [res_dict["ovl_sum_ov"] for res_dict in sim_res["bus"].values()]
+    ax1.scatter(
+        x_coord, num_v, label="Voltage - upper limit", color=col, marker="^", s=120
+    )
+    num_v = [res_dict["ovl_sum_lw"] for res_dict in sim_res["bus"].values()]
+    ax1.scatter(
+        x_coord,
+        num_v,
+        label="Voltage - lower limit",
+        color=(0 / 255, 83 / 255, 116 / 255),
+        marker="v",
+        s=120,
+    )
+    ax1.set_xlabel("")
+    ax1.set_ylabel("Aggregated voltage range violations, in p.u.", color=col)
+    ax1.tick_params(axis="y", labelcolor=col)
+    ax1.set_xticks(x_coord)
+    ax1.set_xticklabels(sim_res["bus"].keys(), rotation=45, ha="right")
+    # seconds axis with trafo loading
+    col = (109 / 255, 131 / 255, 0 / 255)
+    ax2 = ax1.twinx()
+    num_v = [res_dict["ovl_work_kWh"] / 1000 for res_dict in sim_res["line"].values()]
+    ax2.scatter(x_coord, num_v, label="Loading", color=col, s=100)
+    ax2.set_ylabel("Aggregated line & transformer overloading, in MWh", color=col)
+    ax2.tick_params(axis="y", labelcolor=col)
+    # adds
+    ax1.legend(loc="upper right")
+    # fig.get_axes()[0].set_rasterized(True)
+    plt.savefig(
+        os.path.join("outputs", "comp", "comp_plot_viol.png"),
+        dpi=300,
+        format="png",
+        bbox_inches="tight",
+    )
+
+
+def comp_results_plots_extrema(sim_res):
+    # plot for comparison of max/min values
+    fig, ax1 = plt.subplots(figsize=(10, 4))
+    ax1.grid()
+    x_coord = np.arange(len(sim_res["bus"].keys()))
+    # first axis with voltage values
+    col = (102 / 255, 180 / 255, 211 / 255)
+    max_v = [res_dict["max_val"] for res_dict in sim_res["bus"].values()]
+    min_v = [res_dict["min_val"] for res_dict in sim_res["bus"].values()]
+    ax1.bar(x_coord, np.array(max_v) - np.array(min_v), bottom=min_v, color=col)
+    ax1.set_xlabel("")
+    ax1.set_ylabel("Range for bus voltage magnitude, in p.u.", color=col)
+    ax1.tick_params(axis="y", labelcolor=col)
+    ax1.set_xticks(x_coord)
+    ax1.set_xticklabels(sim_res["bus"].keys(), rotation=45, ha="right")
+    # seconds axis with trafo loading
+    col = (109 / 255, 131 / 255, 0 / 255)
+    ax2 = ax1.twinx()
+    ax2.scatter(
+        x_coord,
+        [res_dict["max_val"] for res_dict in sim_res["line"].values()],
+        color=col,
+    )
+    ax2.set_ylabel("Maximum transformer loading, in %", color=col)
+    ax2.tick_params(axis="y", labelcolor=col)
+    # adds
+    plt.savefig(
+        os.path.join("outputs", "comp", "comp_plot_extr.png"),
+        dpi=300,
+        format="png",
+        bbox_inches="tight",
+    )
+
+
+def comp_results_plots_agents(sim_res):
+    # plot for comparison of costs and self-consumption (compared to reference case)
+    fig, ax1 = plt.subplots(figsize=(8, 3))
+    ax1.grid()
+    x_names_w_ref = list(sim_res["bus"].keys())
+    x_ref = x_names_w_ref[0]
+    x_names = x_names_w_ref[1:]
+    x_coord = np.arange(len(x_names))
+    # first axis with costs
+    col = (102 / 255, 180 / 255, 211 / 255)
+    num_v = [
+        (
+            sim_res["agents"][res_name]["mean_cost"]
+            - sim_res["agents"][x_ref]["mean_cost"]
+        )
+        for res_name in x_names
+    ]
+    ax1.scatter(x_coord, num_v, color=col, s=120)
+    ax1.set_xlabel("")
+    ax1.set_ylabel("Change in average cost, in EUR", color=col)
+    ax1.tick_params(axis="y", labelcolor=col)
+    ax1.set_xticks(x_coord)
+    ax1.set_xticklabels(x_names, rotation=45, ha="right")
+    # seconds axis with self-consumption
+    col = (109 / 255, 131 / 255, 0 / 255)
+    ax2 = ax1.twinx()
+    num_v = [
+        (sim_res["agents"][res_name]["mean_sc"] - sim_res["agents"][x_ref]["mean_sc"])
+        for res_name in x_names
+    ]
+    ax2.scatter(x_coord, num_v, color=col, s=100)
+    ax2.set_ylabel("Change in average self-consumption, in %", color=col)
+    ax2.tick_params(axis="y", labelcolor=col)
+    fig.get_axes()[0].set_rasterized(True)
+    plt.savefig(
+        os.path.join("outputs", "comp", "comp_plot_agents.png"),
+        dpi=300,
+        format="png",
+        bbox_inches="tight",
+    )
+
+    # plot for comparison of costs and energy sums (compared to reference case)
+    fig, ax1 = plt.subplots(figsize=(8, 3))
+    ax1.grid()
+    x_names_w_ref = list(sim_res["bus"].keys())
+    x_ref = x_names_w_ref[0]
+    x_names = x_names_w_ref[1:]
+    x_coord = np.arange(len(x_names))
+    # first axis with costs
+    col = (109 / 255, 131 / 255, 0 / 255)
+    num_v = [
+        (
+            sim_res["agents"][res_name]["mean_cost"]
+            - sim_res["agents"][x_ref]["mean_cost"]
+        )
+        for res_name in x_names
+    ]
+    ax1.scatter(x_coord, num_v, color=col, s=120)
+    ax1.set_xlabel("")
+    ax1.set_ylabel("Change in average cost, in EUR", color=col)
+    ax1.tick_params(axis="y", labelcolor=col)
+    ax1.set_xticks(x_coord)
+    ax1.set_xticklabels(x_names, rotation=45, ha="right")
+    # seconds axis with energy sums
+    col = (102 / 255, 180 / 255, 211 / 255)
+    ax2 = ax1.twinx()
+    num_v = [
+        (
+            sum(sim_res["agents"][res_name]["amount_edemand_list"])
+            - sum(sim_res["agents"][x_ref]["amount_edemand_list"])
+        )
+        for res_name in x_names
+    ]
+    ax2.scatter(x_coord, num_v, label="Energy Demand", marker="^", color=col, s=100)
+    num_v = [
+        (
+            sum(sim_res["agents"][res_name]["amount_efeedin_list"])
+            - sum(sim_res["agents"][x_ref]["amount_efeedin_list"])
+        )
+        for res_name in x_names
+    ]
+    ax2.scatter(x_coord, num_v, label="Energy Feedin", marker="v", color=col, s=100)
+    ax2.set_ylabel("Change in aggregated energy, in %", color=col)
+    ax2.tick_params(axis="y", labelcolor=col)
+    fig.get_axes()[0].set_rasterized(True)
+    plt.savefig(
+        os.path.join("outputs", "comp", "comp_plot_agents.png"),
+        dpi=300,
+        format="png",
+        bbox_inches="tight",
+    )
+
+
+def comp_results_plots_agents_cost(sim_res):
+    # plot for comparison of costs and self-consumption (compared to reference case)
+    fig, ax1 = plt.subplots(figsize=(8, 3))
+    ax1.grid()
+    x_names_w_ref = list(sim_res["bus"].keys())
+    x_ref = x_names_w_ref[0]
+    x_names = x_names_w_ref[1:]
+    x_coord = np.arange(len(x_names))
+    # first axis with costs
+    col = (102 / 255, 180 / 255, 211 / 255)
+    num_v = [
+        (
+            (
+                sim_res["agents"][res_name]["mean_cost"]
+                - sim_res["agents"][x_ref]["mean_cost"]
+            )
+            * 100
+        )
+        for res_name in x_names
+    ]
+    for idx in x_coord:
+        ax1.text(
+            x=idx,
+            y=num_v[idx] + 17,
+            s=str(round(num_v[idx])),
+            color=col,
+            horizontalalignment="center",
+            verticalalignment="center",
+        )
+    ax1.scatter(x_coord, num_v, color=col, s=120)
+    ax1.set_xlabel("")
+    ax1.set_ylabel("Change in average cost, in ct", color=col)
+    ax1.tick_params(axis="y", labelcolor=col)
+    ax1.set_xticks(x_coord)
+    ax1.set_xticklabels(x_names, rotation=45, ha="right")
+    ax1.set_ylim(top=max(num_v) * 1.15)
+    plt.savefig(
+        os.path.join("outputs", "comp", "comp_plot_costs.png"),
+        dpi=300,
+        format="png",
+        bbox_inches="tight",
+    )
+
+
+def comp_results_plots_agents_energy(sim_res):
+    # plot for energy sums
+    fig, ax1 = plt.subplots(figsize=(18, 4))
+    ax1.grid()
+    x_coord = np.arange(len(sim_res["agents"].keys()))
+    num_v = [sum(res_dict["amount_e_cons"]) for res_dict in sim_res["agents"].values()]
+    ax1.scatter(
+        x_coord,
+        num_v,
+        label="Consumption",
+        color=(102 / 255, 180 / 255, 211 / 255),
+        marker="v",
+        s=120,
+    )
+    for i, value in enumerate(num_v):
+        plt.text(i, value + 100, str(value), ha="center", va="bottom")
+    num_v = [-sum(res_dict["amount_e_gen"]) for res_dict in sim_res["agents"].values()]
+    ax1.scatter(
+        x_coord,
+        num_v,
+        label="Generation",
+        color=(0 / 255, 83 / 255, 116 / 255),
+        marker="v",
+        s=120,
+    )
+    for i, value in enumerate(num_v):
+        plt.text(i, value + 100, str(value), ha="center", va="bottom")
+    num_v = [
+        -sum(res_dict["amount_efeedin_list"]) for res_dict in sim_res["agents"].values()
+    ]
+    ax1.scatter(
+        x_coord,
+        num_v,
+        label="Feedin",
+        color=(102 / 255, 180 / 255, 211 / 255),
+        marker="^",
+        s=120,
+    )
+    for i, value in enumerate(num_v):
+        plt.text(i, value + 100, str(value), ha="center", va="bottom")
+    num_v = [
+        sum(res_dict["amount_edemand_list"]) for res_dict in sim_res["agents"].values()
+    ]
+    ax1.scatter(
+        x_coord,
+        num_v,
+        label="Grid Consumption",
+        color=(0 / 255, 83 / 255, 116 / 255),
+        marker="^",
+        s=120,
+    )
+    for i, value in enumerate(num_v):
+        plt.text(i, value + 100, str(value), ha="center", va="bottom")
+    # plt.ylim((min(list_ss) - 0.1, max(max(list_ss) + 0.3, 1)))
+    ax1.set_xlabel("")
+    ax1.set_ylabel("Average energy, in kWh")
+    ax1.tick_params(axis="y")
+    ax1.set_xticks(x_coord)
+    ax1.set_xticklabels(sim_res["agents"].keys(), rotation=45, ha="right")
+    ax1.legend(loc="upper right")
+    # ax1.set_rasterized(True)
+    plt.savefig(
+        os.path.join("outputs", "comp", "comp_plot_energy.png"),
+        dpi=300,
+        format="png",
+        bbox_inches="tight",
+    )
 
 
 def main():
@@ -326,6 +687,14 @@ def main():
 
     print(f"Create Overview Table for Sim Results: ...")
     comp_results_table(sim_res)
+    print(f"Done!")
+
+    print(f"Create Comparing Plots for Sim Results: ...")
+    comp_results_plots_violations(sim_res)
+    comp_results_plots_extrema(sim_res)
+    comp_results_plots_agents(sim_res)
+    comp_results_plots_agents_cost(sim_res)
+    comp_results_plots_agents_energy(sim_res)
     print(f"Done!")
 
 
